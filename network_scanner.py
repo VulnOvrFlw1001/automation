e5205bf3310ee099e2a4c544e2e48cd8beed2374
Joseph <joseph.hansjpedronan@gmail.com>
2026-08-29 17:54:22 -0700

    scanner

---
+++ network_scanner.py
@@ -0,0 +1,8 @@
+import scapy.all as scapy
+import socket
+
+def scan(ip):
+    arp_request = scapy.ARP(pdst=ip)
+    broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
+    packet = broadcast/arp_request
+    answer = scapy.srp(packet, timeout=1, verbose=False)[0]
