ad8d3595f4defee3f37922037282c55dda2ff331
Joseph <joseph.hansjpedronan@gmail.com>
2026-09-04 17:09:26 -0700

    port_scanner

--- port_scanner.py
+++ port_scanner.py
@@ -1,6 +1,16 @@
 import socket
 
+def get_banner(sock):
+    pass
+
 def scan_port(target_ip, port): 
     try:
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         sock.settimeout(1)
+        result = sock.connect_ex((target_ip.port))
+        if result == 0:
+            try:
+                service = socket.getservbyport(port, 'tcp')
+            except:
+                service = 'Unknown'
+            banner = get_banner(sock)
