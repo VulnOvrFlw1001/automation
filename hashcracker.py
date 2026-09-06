8864d5ae25cbf26a192aabfbd63970591a3e0145
VulnOvrFlw1001 <hansjpedronan@gmail.com>
2026-09-06 13:27:11 -0700

    Add files via upload

--- hashcracker.py
+++ hashcracker.py
@@ -2,6 +2,7 @@ import hashlib
 import itertools
 import string
 from concurrent.futures import ThreadPoolExecutor
+from tqdm import tqdm
 
 hash_name = [
     'md5',
@@ -20,7 +21,10 @@ def generate_passwords(min_length, max_length, characters):
         for pwd in itertools.product(characters, repeat=length):
             yield ''.join(pwd)
 
-def crack_hash(hash, wordlist=None, hash_type='md5', min_length=0, characters=string.ascii_letters + string.digits, max_workers=4):
+def check_hash(hash_fn, password, target_hash):
+    return hash_fn(password.encode()).hexdigest() == target_hash
+
+def crack_hash(hash, wordlist=None, hash_type='md5', min_length=0, max_length=0, characters=string.ascii_letters + string.digits, max_workers=4):
     hash_fn = getattr(hashlib, hash_type, None)
     if hash_fn is None or hash_type not in hash_name:
         raise ValueError(f'[!] Invalid hash type: {hash_type} supported are {hash_name}')
@@ -30,4 +34,23 @@ def crack_hash(hash, wordlist=None, hash_type='md5', min_length=0, characters=st
             lines = f.read()
             total_lines = len(lines)
             print(f"[*] Cracking hash {hash} using {hash_type} with a list of {total_lines} passwords.")
-    
+
+            with ThreadPoolExecutor(max_workers=max_workers) as executor:
+                futures = {executor.submit(check_hash, hash_fn, line.strip(), hash): line for line in lines}
+                for future in tqdm(futures, total=total_lines, desc="Cracking hash"):
+                    if future.result():
+                        return futures[future].strip()
+    elif min_length > 0 and max_length > 0 :
+        total_combinations = sum(len(characters) ** length for length in range(min_length, max_length + 1))
+        print(f'[*] Cracking hash {hash} using {hash_type} with generated passwords of lengths from  {min_length} to {max_length}. Total combinations: {total_combinations}')
+
+        with ThreadPoolExecutor(max_workers=max_workers) as executor:
+            futures = []
+            with tqdm(total_combinations, desc='Generating and cracking hash') as pbar:
+                for pwd in generate_passwords(min_length, max_length, characters):
+                    future = executor.submit{check_hash, hash_fn, pwd, hash}
+                    futures.append(future)
+                    pbar.update(1)
+                    if future.result():
+                        return pwd
+    return None
